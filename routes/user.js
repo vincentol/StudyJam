c0edf0bbdd5959b1398c3e48920b796b3cdc9dc3
Joash Duhaylungsod <jeduhayl@ucsd.edu>
2017-02-21 17:47:02 -0800

    Added dropdown links for profile button

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,13 @@
-'use strict';
-
-$(document).ready(function() {
-  initializePage();
-})
-
-function initializePage() {
-  $("#testjs").click(function(e) {
-    window.alert("Oops! This is not yet implemented!");
-  });
-}
-
-
+/* When the user clicks on the button,
+ * toggle between hiding and showing the dropdown content */
 function myFunction() {
-  alert("Oops! This is not yet implemented!");
-}
-
-
-function viewUser() {
 		document.getElementById("myDropdown").classList.toggle("show");
 }
 
+// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-		if (!event.target.matches('.dropbtn')) {
+		if (!event.target.matches('.dropbtn)) {
+
 				var dropdowns = document.getElementsByClassName("dropdown-content");
 				var i;
 				for (i = 0; i < dropdowns.length; i++) {
