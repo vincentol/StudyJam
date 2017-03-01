12bf6674fda0b47c8f4a9c81fa79919352416fb1
VincentOL <vincentohwell@gmail.com>
2017-02-28 16:35:54 -0800

    Implemented Search Bar

--- public/js/study.js
+++ public/js/study.js
@@ -112,6 +112,24 @@ window.onclick = function(event) {
 		}
 }
 
+function searchFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById('searchBar');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchUL");
+  li = ul.getElementsByTagName('li');
+
+  // loop through search results and hide anything that doesn't match
+  for (i=0; i<li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
 var modal = document.getElementById('myModal');
 //var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
